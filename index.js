88dd1c1068a17e27def1c2daf324d3dfdaefa36c
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2024-06-26 22:40:44 -0400

    error with parts array for a bit but managed to fix it

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const express = require("express");
 const bodyParser = require("body-parser");
-const axios = require("axios");
 const cors = require("cors");
 const app = express();
 const PORT = 5001;
-const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,35 +12,42 @@ app.use(bodyParser.json());
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const messages = []; // In-memory storage for messages
+let messages = []; // In-memory storage for messages
 
 app.post("/chat", async (req, res) => {
   const { message } = req.body;
-  const apiKey = process.env.GOOGLE_GEMINI_API_KEY;
 
-  messages.push({
-    role: "user",
-    text: "You are a AI Chatbot who specializes in answering questions about famous Landmarks",
-  });
-
-  try {
-    const chat = model.startChat({
-      history: messages,
-      generationConfig: {
-        maxOutputTokens: 500,
-      },
+  if (message === "") {
+    messages = []; // Reset messages if a new chat is started
+  } else {
+    messages.push({
+      role: "user",
+      parts: [
+        {
+          text: "You are an AI Chatbot who is specialized in answering questions about famous landmarks in detail",
+        },
+      ],
     });
 
-    const result = await chat.sendMessage(message);
-    const response = await result.response;
-    const text = await response.text();
+    try {
+      const chat = model.startChat({
+        history: messages,
+        generationConfig: {
+          maxOutputTokens: 500,
+        },
+      });
 
-    messages.push({ role: "model", text });
-    res.json({ answer: text, messages });
-    console.log(text);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong");
+      const result = await chat.sendMessage(message);
+      const response = await result.response;
+      const text = response.text();
+
+      messages.push({ role: "model", parts: [{ text }] });
+
+      res.json({ answer: text, messages });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Something went wrong");
+    }
   }
 });
 
